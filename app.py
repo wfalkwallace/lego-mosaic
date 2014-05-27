ce16bd564af7ee58c8572b0f98801773c521412e
William Falk-Wallace <wgf2104@columbia.edu>
2014-05-27 15:54:30 -0400

    updates for file basics and travis

--- app.py
+++ app.py
@@ -1,21 +1,57 @@
+import os
 from flask import Flask
+from flask import request
+from flask import redirect
+from flask import url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# image uploading settings
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'GIF'])
+
 # disable this for launch (~~ 'watch')
 app.config["DEBUG"] = True
+# set app-wide uploads folder
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def hello():
-	return "hello world!"
+  return render_template("index.html")
 
-@app.route("/name")
-def name():
-	return "William G. Falk-Wallace"
 
-@app.route("/search/<search_query>")
-def search(search_query):
-	return search_query
+
+def allowed_file_extension(filename):
+    return '.' in filename and 
+    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
