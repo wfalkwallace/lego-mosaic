65f209de1ce857aa0647f0dab1179542386566a4
William Falk-Wallace <wgf2104@columbia.edu>
2014-06-25 13:12:43 -0400

    removes development stuff for deploy branch

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y python
-sudo apt-get install -y python-pip
-sudo apt-get install -y vim
-sudo pip install flask
-sudo pip install -r /vagrant/requirements.txt
-
-echo hello
