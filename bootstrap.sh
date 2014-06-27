53df736c35c059f568c2d4283048b1519fb2f158
William Falk-Wallace <wgf2104@columbia.edu>
2014-06-27 10:54:14 -0400

    add settings source to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y vim
 sudo pip install flask
 sudo apt-get -y build-dep python-imaging
 sudo pip install -r /vagrant/requirements.txt
+source config/settings.prod
 
 echo hello
