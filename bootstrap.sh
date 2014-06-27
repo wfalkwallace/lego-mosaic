628b7d24eb13f02af70c0a38afd1d6eed0ee0cde
William Falk-Wallace <wgf2104@columbia.edu>
2014-06-27 10:49:30 -0400

    bootstrap need the image libs pre-pil install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y python
 sudo apt-get install -y python-pip
 sudo apt-get install -y vim
 sudo pip install flask
+sudo apt-get -y build-dep python-imaging
 sudo pip install -r /vagrant/requirements.txt
 
 echo hello
